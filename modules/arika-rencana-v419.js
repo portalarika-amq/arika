8c0b02e332d6c99a2ba03f78c5e384bd02a9b8fa
portalarika-amq <hellenastde@gmail.com>
2026-06-09 08:06:57 +0900

    Delete modules/arika-rencana-v419.js
    Delete modules/arika-rencana-v419.js

--- modules/arika-rencana-v419.js
+++
@@ -1,8 +0,0 @@
-/* ARIKA v419 - Rencana lazy module */
-(function(){
-  'use strict';
-  if(window.__ARIKA_RENCANA_MODULE_V419__) return;
-  window.__ARIKA_RENCANA_MODULE_V419__ = true;
-  function init(){ try{ if(window.renderRencanaPribadi) window.renderRencanaPribadi({ fromModule:'rencana-v419' }); }catch(e){ console.warn(e); } }
-  window.arikaInitModule_rencana = init;
-})();
